2d4e822b5fb3e4c7fec7f382b0bca9365db233f8
peter <+>
2022-09-07 12:45:13 -0400

    commit tree 16ba61655f0844932c881a8f5063791f4981ea1e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+// const labData = {
+//     name: 'Steve',
+//     email: 'steve@steve.com',
+// };
+
+function submitData(name,email){
+    return fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email
+    })
+
+    })
+    .then(res => res.json())
+    // .then(data => submitData(data))
+}
